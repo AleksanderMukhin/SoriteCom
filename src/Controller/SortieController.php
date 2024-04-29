c642491ee9b9c7b8a39e5cfa2812b1fe6ad173b7
awangchi <arthur.wangchibrac@gmail.com>
2024-04-29 13:47:25 +0200

    fix bugs sorties

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -10,17 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Campus;
 use App\Entity\Sortie;
 
-/**
- * @Route("/accueil", name="accueil")
- */
 class SortieController extends AbstractController
 {
-    #[Route("/", "affichage")]
+    #[Route("/accueil", "accueil")]
     public function accueil(): Response
     {
         return $this->render('main/accueil.html.twig');
     }
-    #[Route("/test", "affichage_test")]
+    #[Route("/accueil/test", "accueil_test")]
     public function test(): Response
     {
         /*
@@ -29,8 +26,9 @@ class SortieController extends AbstractController
         $SortieRepository = $this->getDoctrine()->getRepository(Campus::class);
         $sortie = $SortieRepository->findAll();
         */
-        $campuses = ["id" => 1,
-            "c_nom"=>"Saint Herblain"
+        $campuses = [
+            ["id" => 1, "c_nom" => "Saint Herblain"],
+            ["id" => 2, "c_nom" => "Autre Campus"]
         ];
 
         $sortie = [
@@ -42,22 +40,21 @@ class SortieController extends AbstractController
             "s_organisateur" => "Jean Jean",
         ];
 
-        return $this->render('main/accueil.html.twig',[
-            "uneSortie" => $sortie,
-            'campuses' => $campuses,]);
+        return $this->render('main/morceaux/table_sorties.html.twig', [
+            'uneSortie' => $sortie,
+            'campuses' => $campuses,
+        ]);
     }
 
-    /**
-     * @Route("/liste-sortie", name="liste_sortie")
-     */
+    #[Route("/accueil/liste", "accueil_liste")]
     public function getSorties(Request $request): Response
     {
         $campusId = $request->query->get('campusId');
         // Récupérer les sorties associées au campus sélectionné
-        $sorties = $this->getDoctrine()->getRepository(Event::class)->findBy(['campus' => $campusId]);
+        $sorties = $this->getDoctrine()->getRepository(Sortie::class)->findBy(['campus' => $campusId]);
 
         // Rendre le template Twig pour le tableau de sorties
-        return $this->render('morceaux/events_table.html.twig', [
+        return $this->render('main/morceaux/table_sorties.html.twig', [
             'uneSortie' => $sorties,
         ]);
     }
